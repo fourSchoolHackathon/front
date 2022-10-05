603a2b3b21ef324110224898b15f14516b164738
chanwoo00106 <chan00106@naver.com>
2022-10-05 20:06:26 +0900

    Add error handling

--- fe/src/components/Login/index.js
+++ fe/src/components/Login/index.js
@@ -4,7 +4,11 @@ import * as S from './style'
 
 const Main = () => {
   const [error, setError] = useState()
-  const { register, handleSubmit } = useForm()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm()
 
   const onSubmit = form => {
     console.log(form)
@@ -19,26 +23,44 @@ const Main = () => {
             <S.Label>아이디</S.Label>
             <S.Input
               {...register('account_id', {
-                maxLength: 24,
-                minLength: 6,
-                required: true
+                maxLength: {
+                  value: 24,
+                  message: '글자 길이는 최대 24자입니다'
+                },
+                minLength: { value: 6, message: '글자 길이는 최소 6자입니다' },
+                required: '아이디는 필수값입니다'
               })}
               placeholder="아이디를 입력해주세요"
               type="text"
+              aria-invalid={errors.account_id ? 'true' : 'false'}
             />
+            {errors.account_id && (
+              <S.Label style={{ color: 'red' }} role="alert">
+                {errors.account_id?.message}
+              </S.Label>
+            )}
           </S.InputWrapper>
 
           <S.InputWrapper>
             <S.Label>비밀번호</S.Label>
             <S.Input
               {...register('password', {
-                maxLength: 24,
-                minLength: 8,
-                required: true
+                maxLength: {
+                  value: 24,
+                  message: '글자 길이는 최대 24자입니다'
+                },
+                minLength: { value: 8, message: '글자 길이는 최소 8자입니다' },
+                required: '비밀번호는 필수값입니다'
               })}
               placeholder="비밀번호를 입력해주세요"
               type="password"
+              aria-invalid={errors.password ? 'true' : 'false'}
             />
+            {errors.password && (
+              <S.Label style={{ color: 'red' }} role="alert">
+                {errors.password?.message}
+              </S.Label>
+            )}
           </S.InputWrapper>
         </S.LoginWrapper>
 
