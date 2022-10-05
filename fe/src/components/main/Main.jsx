cd857be7439ee6160d7f632126ea2b67038163cd
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-10-05 23:30:24 +0900

    [UPDATE] select service

--- fe/src/components/main/Main.jsx
+++ fe/src/components/main/Main.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import * as M from './Main.style'
 
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from 'react-router-dom'
 
 import { useRecoilState } from 'recoil'
 import { storedLocation } from '../../stores/location/location'
@@ -9,7 +9,7 @@ import { storedLocation } from '../../stores/location/location'
 const Main = () => {
   const [location, setLocation] = useRecoilState(storedLocation)
 
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   function getLocation() {
     if (navigator.geolocation) {
@@ -22,7 +22,7 @@ const Main = () => {
             lat: position.coords.latitude,
             lng: position.coords.longitude
           })
-          navigate("/req")
+          navigate('/req')
         },
         function (error) {
           console.error(error)
@@ -38,9 +38,48 @@ const Main = () => {
     }
   }
 
+//   useEffect(() => {
+//     console.log(location)
+//   }, [location])
+
+  // -------
+
+  const [gender, setGender] = useState('')
+  const [selected, setSelected] = useState([])
+  const [duration, setDuration] = useState('단기')
+  const [time, setTime] = useState(new Date().toISOString().slice(0, -6))
+  //   const [time,setTime] = useState(new Date().toISOString().slice(0,-14))
+
+  const service = [
+    '이동보조',
+    '말벗',
+    '주변정리',
+    '식사보조',
+    '목욕보조',
+    '배변보조',
+    '체위변경',
+    '투약보조',
+    '운동보조',
+    '기타'
+  ]
+
+  /**인자값으로 들어온 서비스가 이미 선택된 것이라면 true */
+  function findService(service){
+    return selected.includes(service);
+  }
+  function handleService(service){
+    // 이미 있었다면 취소
+    if (findService(service)){
+        setSelected((prev) => prev.filter((e) => e !== service))
+    } else { // 없으니 추가
+        setSelected((prev) => [...prev,service])
+    }
+  }
+  function dateHandler(e) {}
+
   useEffect(() => {
-    console.log(location)
-  },[location])
+    console.log(selected)
+  },[selected])
 
   return (
     <M.Wrapper>
@@ -60,10 +99,83 @@ const Main = () => {
 
       <M.CategoryReqWrapper>
         <M.CategoryComments>
-            <h3>카테고리로 요청</h3>
-            <p>카테고리 선택으로 정확하게 도움을 요청하세요</p>
+          <h3>카테고리로 요청</h3>
+          <p>카테고리 선택으로 정확하게 도움을 요청하세요</p>
         </M.CategoryComments>
+
+        <M.CategoryWrapper>
+          <M.GenderCategory>
+            <h3>성별</h3>
+            <M.InputWrapper>
+              <M.LeftInput
+                type="radio"
+                id="select"
+                name="gender"
+                onClick={() => setGender('남자')}
+              />
+              <label htmlFor="select">남성</label>
+              <M.RightInput
+                type="radio"
+                id="select2"
+                name="gender"
+                onClick={() => setGender('여성')}
+              />
+              <label htmlFor="select2">여성</label>
+            </M.InputWrapper>
+          </M.GenderCategory>
+
+          <h3>서비스</h3>
+          <M.Categorys>
+            {service.map(i => (
+              <M.Category 
+              onClick={() => handleService(i)}
+              isExist={findService(i)}
+              key={i}
+              >{i}</M.Category>
+            ))}
+          </M.Categorys>
+
+          <h3>시간</h3>
+          <M.DateWrapper>
+            <M.InputWrapper>
+              <M.LeftInput
+                type="radio"
+                id="select3"
+                name="duration"
+                onClick={() => setDuration('단기')}
+                defaultChecked
+              />
+              <label htmlFor="select3">단기</label>
+              <M.RightInput
+                type="radio"
+                id="select4"
+                name="duration"
+                onClick={() => setDuration('장기')}
+              />
+              <label htmlFor="select4">장기</label>
+            </M.InputWrapper>
+
+            <M.DateInputWrapper>
+              {
+                duration === "단기" ?
+                <input type="date" defaultValue={new Date().toISOString().slice(0,-14)} />
+                :
+                <></>
+
+              }
+
+              {/* <input type="date" defaultValue={new Date().toISOString().slice(0,-14)} onChange={(e) => dateHandler(e,)} /> */}
+
+              {/* <input
+                type="date"
+                defaultValue={time}
+                onChange={e => dateHandler(e)}
+              /> */}
+            </M.DateInputWrapper>
+          </M.DateWrapper>
+        </M.CategoryWrapper>
       </M.CategoryReqWrapper>
+      <M.Temp />
     </M.Wrapper>
   )
 }
--- fe/src/components/main/Main.style.js
+++ fe/src/components/main/Main.style.js
@@ -1,89 +1,173 @@
-import styled from "styled-components"
+import styled, { css } from 'styled-components'
 
 export const Wrapper = styled.div`
-    width:100%;
-    height:100%;
-    display:flex;
-    flex-direction:column;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
 `
 
 export const HeaderWrapper = styled.div`
-    width:100%;
-    height:60px;
-    border-bottom: 1px solid rgb(239, 239, 239);
-    display:flex;
-    align-items:center;
-    > div{
-        margin-left:20px;
-        font-weight:bold;
-    }
+  width: 100%;
+  height: 60px;
+  border-bottom: 1px solid rgb(239, 239, 239);
+  display: flex;
+  align-items: center;
+  > div {
+    margin-left: 20px;
+    font-weight: bold;
+  }
 `
 
 export const SimpleReqWrapper = styled.div`
-    width:100%;
-    height:300px;
-    background-color:#ff8450;
-    
-
+  width: 100%;
+  height: 300px;
+  background-color: #ff8450;
 `
 
 export const SimpleComments = styled.div`
-    width:100%;
-    height:100px;
-    box-sizing:border-box;
-    display:flex;
-    flex-direction:column;
-    *{
-        margin-left:20px;
-        color:white;
-    }
-    >h3{
-        margin-bottom:0px;
-    }
-    >p{
-        margin-top:6px;
-        font-size:13px;
-    }
+  width: 100%;
+  height: 100px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  * {
+    margin-left: 20px;
+    color: white;
+  }
+  > h3 {
+    margin-bottom: 0px;
+  }
+  > p {
+    margin-top: 6px;
+    font-size: 13px;
+  }
 `
 
 export const ButtonWrapper = styled.div`
-    width:100%;
-    height:160px;
-    display:flex;
-    justify-content:center;    
-    align-items:center;
-    >button{
-        color:#ff8450;
-        width:200px;
-        height:80px;
-        outline:none;
-        border:none;
-        border-radius:15px;
-        background-color:white;
-        font-size:20px;
-        font-weight:bold;
-    }
+  width: 100%;
+  height: 160px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  > button {
+    color: #ff8450;
+    width: 200px;
+    height: 80px;
+    outline: none;
+    border: none;
+    border-radius: 15px;
+    background-color: white;
+    font-size: 20px;
+    font-weight: bold;
+  }
 `
 
 export const CategoryReqWrapper = styled.div`
-    width:100%;
+  width: 100%;
 `
 export const CategoryComments = styled.div`
-    width:100%;
-    height:100px;
-    box-sizing:border-box;
-    display:flex;
-    flex-direction:column;
-    *{
-        margin-left:20px;
-        color:black
-    }
-    >h3{
-        margin-bottom:0px;
-    }
-    >p{
-        font-size:13px;
-        margin-top:6px;
-    }
+  width: 100%;
+  height: 90px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  * {
+    margin-left: 20px;
+    color: black;
+  }
+  > h3 {
+    margin-bottom: 0px;
+  }
+  > p {
+    font-size: 13px;
+    margin-top: 6px;
+  }
 `
 
+export const CategoryWrapper = styled.div`
+  width: 100%;
+  /* height: 300px; */
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+
+  h3 {
+    margin: 0px 20px;
+  }
+`
+
+export const GenderCategory = styled.div`
+  width: 100%;
+`
+export const InputWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin: 20px 0px;
+`
+
+const Input = styled.input`
+  display: none;
+  + label {
+    cursor: pointer;
+    height: 100%;
+    width: 30%;
+    font-size: 20px;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1.9px solid #ff8450;
+    background-color: #fff;
+    color: #ff8450;
+  }
+  :checked + label {
+    background-color: #ff8450;
+    color: #fff;
+  }
+`
+
+export const LeftInput = styled(Input)`
+  + label {
+    border-radius: 7px 0px 0px 7px;
+  }
+`
+export const RightInput = styled(Input)`
+  + label {
+    border-radius: 0px 7px 7px 0px;
+  }
+`
+
+export const Categorys = styled.div`
+  margin: 10px 0px 20px 20px;
+  display: flex;
+  flex-wrap: wrap;
+`
+export const Category = styled.div`
+  padding: 8px 16px;
+  font-size: 14px;
+  border-radius: 15px;
+  box-shadow: 4px 4px 15px rgba(0, 0, 0, 0.1);
+  margin: 7px;
+  /* ${(props) => props.isExist ? css`
+    background-color:#ff8450;
+    color:white;
+  `:null} */
+  color:${(props) => props.isExist ? "white" : "black"};
+  background-color:${(props) => props.isExist ? "#ff8450" : "white"};
+`
+
+export const DateWrapper = styled.div`
+  width: 100%;
+  display:flex;
+  flex-direction:column;
+`
+export const DateInputWrapper = styled.div`
+    margin:10px 0px 20px 20px;
+`
+
+export const Temp = styled.div`
+  width: 100%;
+  height: 70px;
+`
