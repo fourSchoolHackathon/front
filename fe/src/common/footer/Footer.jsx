afbf7ebef02b62be296cd3bb10a0240a948cce0e
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-10-05 15:11:23 +0900

    setRoute

---
+++ fe/src/common/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const index = () => {
+    return (
+        <div>
+            푸터
+        </div>
+    );
+};
+
+export default index;
---
+++ fe/src/common/navbar/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Index = () => {
+    return (
+        <div>
+            네브바
+        </div>
+    );
+};
+
+export default Index;
